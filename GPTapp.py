64f6d82aa492847cc894adbcfa2cdf005122b349
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-05 09:13:56 +0900

    June

--- GPTapp.py
+++ GPTapp.py
@@ -18,21 +18,21 @@ st.session_state['counter']=counter
 st.session_state['status']=status
 
 if status==0:
-    prob = st.button("問題 (quiz)")
-    if prob:
-        quiz_st="This is ...."+str(status)+"-"+str(counter)
-        answ_st="The answer is ...."+str(status)+"-"+str(counter)
-        status=1
-        st.session_state['status']=status
-        st.session_state['quiz_st']=quiz_st
-        st.session_state['answ_st']=answ_st
-        
+    st.button("問題 (quiz)")
+    quiz_st="This is ...."+str(status)+"-"+str(counter)
+    answ_st="The answer is ...."+str(status)+"-"+str(counter)
+    status=1
+    st.session_state['status']=status
+    st.session_state['quiz_st']=quiz_st
+    st.session_state['answ_st']=answ_st
+
+    
 elif status==1:
-  quiz_st=st.session_state['quiz_st']
-  answ_st=st.session_state['answ_st']
-  st.write("Q: "+quiz_st)
-  st.write("A: "+answ_st)
-  status=0
-  st.session_state['status']=status
-  st.button("次へ (next)")
+    quiz_st=st.session_state['quiz_st']
+    answ_st=st.session_state['answ_st']
+    st.write("Q: "+quiz_st)
+    st.write("A: "+answ_st)
+    status=0
+    st.session_state['status']=status
+    st.button("次へ (next)")
   
