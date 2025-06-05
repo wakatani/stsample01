9bd152b8c1b39cd6cf17d66d0d3baad0226d02b5
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-05 09:21:49 +0900

    June

--- GPTapp.py
+++ GPTapp.py
@@ -19,15 +19,13 @@ st.session_state['status']=status
 
 if status==0:
     st.button("問題 (quiz)")
-    quiz_st="This is ...."+str(status)+"-"+str(counter)
-    answ_st="The answer is ...."+str(status)+"-"+str(counter)
     status=1
     st.session_state['status']=status
-    st.session_state['quiz_st']=quiz_st
-    st.session_state['answ_st']=answ_st
 
     
 elif status==1:
+    quiz_st="This is ...."+str(status)+"-"+str(counter)
+    answ_st="The answer is ...."+str(status)+"-"+str(counter)
     quiz_st=st.session_state['quiz_st']
     answ_st=st.session_state['answ_st']
     st.write("Q: "+quiz_st)
@@ -49,7 +47,7 @@ elif status==2:
     answ_st=st.session_state['answ_st']
     st.write("Q: "+quiz_st)
     st.write("A: "+answ_st)
-    status=0
+    status=1
     st.session_state['status']=status
     st.button("次へ (next)")
   
