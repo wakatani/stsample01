b821f8abe8657b84b7a361748d0a4db96b53b29a
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-05 09:01:10 +0900

    June

--- GPTapp.py
+++ GPTapp.py
@@ -12,24 +12,29 @@ if 'counter' in st.session_state:
     counter=st.session_state['counter']
 else:
     counter=0
-
-    
-st.write("### 問題のページ "+str(status)+" "+str(counter))
+st.write("### 問題のページ "+str(status)+"-"+str(counter))
 counter +=1
 st.session_state['counter']=counter
 st.session_state['status']=status
 
-prob = st.button("問題 (quiz)")
-
-if prob:
-  like_streamlit = st.checkbox("Streamlitが好きですか？")
+if status==0:
+    prob = st.button("問題 (quiz)")
+    if prob:
+        like_streamlit = st.checkbox("Streamlitが好きですか？")
+        status=1
+        st.session_state['like_streamlit']=like_streamlit
+        st.session_state['status']=status
+        
+elif status==1:
+  like_streamlit=st.session_state['like_streamlit']
   if like_streamlit:
       level = st.radio("どれくらい好きですか？", ("ちょっと", "まあまあ", "とても"))
       st.write(f"レベル：{level}")
   else:
       st.write("これから好きになるかもしれませんね！")
-
-
+  status=0
+  st.session_state['status']=status
+  
 # Input
 input_program = st.text_area('本当に本当の最初のメッセージ')
 input_error = st.text_area('次のメッセージ')
